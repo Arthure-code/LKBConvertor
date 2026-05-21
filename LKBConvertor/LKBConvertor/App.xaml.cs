6b6b298d5075c974427f151af9a9d866167e7c4b
Arthure Lekoubou Djune <2393288@cegeplimoilou.ca>
2026-05-21 06:40:07 +0100

    Création du projet .NET MAUI LKBConvertor

---
+++ LKBConvertor/LKBConvertor/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LKBConvertor
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LKBConvertor/LKBConvertor/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LKBConvertor
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
