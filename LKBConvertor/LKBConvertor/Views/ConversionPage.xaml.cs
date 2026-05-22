26271c6d90bd6e40931ba3df03984b7f4d225ab7
Arthure Lekoubou Djune <2393288@cegeplimoilou.ca>
2026-05-22 05:29:53 +0100

    Création de ConversionPage.xaml avec zone fichier et progression

---
+++ LKBConvertor/LKBConvertor/Views/ConversionPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using LKBConvertor.Models;
+using LKBConvertor.ViewModels;
+
+namespace LKBConvertor.Views;
+
+public partial class ConversionPage : ContentPage
+{
+    public ConversionPage(ConversionType type)
+    {
+        InitializeComponent();
+
+        var vm = new ConversionViewModel(type);
+        Resources["vm"] = vm;
+
+        Title = type == ConversionType.WordVersPdf
+            ? "Word → PDF"
+            : "PDF → RTF";
+    }
+}
