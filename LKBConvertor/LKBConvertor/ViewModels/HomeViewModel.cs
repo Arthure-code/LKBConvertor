0d90fd72fd67c836d08609c1496409862e92fd40
Arthure Lekoubou Djune <2393288@cegeplimoilou.ca>
2026-05-22 05:16:08 +0100

    Création de HomeViewModel avec les commandes de navigation

---
+++ LKBConvertor/LKBConvertor/ViewModels/HomeViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using LKBConvertor.Data;
+using LKBConvertor.Models;
+
+namespace LKBConvertor.ViewModels
+{
+    public class HomeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private LKBDatabase _bd = new LKBDatabase();
+
+        public Command NavigerWordPdfCommande { get; set; }
+        public Command NavigerPdfRtfCommande { get; set; }
+        public Command NavigerHistoriqueCommande { get; set; }
+
+        private List<ConversionHistory> _conversionsRecentes;
+        public List<ConversionHistory> ConversionsRecentes
+        {
+            get { return _conversionsRecentes; }
+            set
+            {
+                _conversionsRecentes = value;
+                OnPropertyChanged(nameof(ConversionsRecentes));
+            }
+        }
+
+        public HomeViewModel()
+        {
+            NavigerWordPdfCommande = new Command(NavigerVersWordPdf);
+            NavigerPdfRtfCommande = new Command(NavigerVersPdfRtf);
+            NavigerHistoriqueCommande = new Command(NavigerVersHistorique);
+        }
+
+        private void NavigerVersWordPdf()
+        {
+            App.Current.MainPage.Navigation.PushAsync(
+                new Views.ConversionPage(ConversionType.WordVersPdf));
+        }
+
+        private void NavigerVersPdfRtf()
+        {
+            App.Current.MainPage.Navigation.PushAsync(
+                new Views.ConversionPage(ConversionType.PdfVersRtf));
+        }
+
+        private void NavigerVersHistorique()
+        {
+            App.Current.MainPage.Navigation.PushAsync(
+                new Views.HistoriquePage());
+        }
+
+        public void ChargerConversionsRecentes()
+        {
+            ConversionsRecentes = _bd.ObtenirRecentes();
+        }
+
+        private void OnPropertyChanged(string nomPropriete)
+        {
+            PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs(nomPropriete));
+        }
+    }
+}
