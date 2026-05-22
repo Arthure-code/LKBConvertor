091488767618349a5c7b04c75eda978d3bab0eea
Arthure Lekoubou Djune <2393288@cegeplimoilou.ca>
2026-05-22 05:02:02 +0100

    Création du modèle ConversionType enum

---
+++ LKBConvertor/LKBConvertor/Models/ConversionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LKBConvertor.Models
+{
+    public enum ConversionType
+    {
+        WordVersPdf = 0,
+        PdfVersRtf = 1
+    }
+}
