b2eeced2b47fa2606588be0daf3f7f354c830dbd
Arthure Lekoubou Djune <2393288@cegeplimoilou.ca>
2026-05-22 05:00:19 +0100

    Configuration de App.xaml.cs avec le chemin BD SQLite

--- LKBConvertor/LKBConvertor/App.xaml.cs
+++ LKBConvertor/LKBConvertor/App.xaml.cs
@@ -1,12 +1,29 @@
-﻿namespace LKBConvertor
+﻿using LKBConvertor.Models;
+using SQLite;
+
+namespace LKBConvertor
 {
     public partial class App : Application
     {
+        public static string CheminBD;
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            var nomBD = "lkbconvertor_db.sqlite";
+            var repertoire = System.Environment.GetFolderPath(
+                System.Environment.SpecialFolder.Personal);
+            var cheminAcces = Path.Combine(repertoire, nomBD);
+
+            CheminBD = cheminAcces;
+
+            using (var conn = new SQLiteConnection(cheminAcces))
+            {
+                conn.CreateTable<ConversionHistory>();
+            }
+
+            MainPage = new NavigationPage(new Views.HomePage());
         }
     }
 }
